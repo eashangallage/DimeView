7fecdc669c61df527c40be19181eee2df8d071c0
Eashan Gallage <eashanpol@gmail.com>
2026-02-15 22:34:42 -0500

    Bump version to 3.0.0

--- src/dimeview/__init__.py
+++ src/dimeview/__init__.py
@@ -1,6 +1,6 @@
 # src/dimeview/__init__.py
 
-__version__ = "2.0.1"
+__version__ = "3.0.0"
 __author__ = "Eashan Gallage"
 __email__ = "eashanpol@gmail.com"
 # package-wide logger, constants, etc. could live here too
